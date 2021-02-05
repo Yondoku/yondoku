22b258125076e37edd61f964dbfc18867eecf248
Yondoku <69292387+Yondoku@users.noreply.github.com>
2021-02-04 20:58:52 -0900

    Update copy.js

--- commands/copy.js
+++ commands/copy.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'copy',
 	description: 'Copies text.',
   usage: '<textString>',
+	args: true,
 	execute(message, args, Discord) {
 
 		const copyEmbed = new Discord.MessageEmbed().setColor('#5d83a2').setAuthor(`${message.author.tag}`, `${message.author.avatarURL()}`).setDescription(args.join(" "));
