92f14a80c8e373770fdc7fa7750fa601734c7d36
Yondoku <abcandy654@gmail.com>
2021-02-09 18:30:59 -0900

    Update messageboard.js

--- commands/messageboard.js
+++ commands/messageboard.js
@@ -5,7 +5,7 @@ module.exports = {
   usage: '[textString]',
   execute(message, args, Discord) {
 if (!args.length) {
-  const messageboardEmbed = new Discord.MessageEmbed().setColor('#5d83a2').setAuthor(`${message.author.tag}`, `${message.author.avatarURL()}`).setTitle('Message board says...').setDescription(fs.readFileSync('messageboard.txt'));
+  const messageboardEmbed = new Discord.MessageEmbed().setColor('#5d83a2').setAuthor(`${message.author.tag}`, `${message.author.avatarURL()}`).setTitle('Message board says...').setDescription(fs.readFile('messageboard.txt'));
   return message.channel.send({ embed: messageboardEmbed });
 }
   const messageboardsetEmbed = new Discord.MessageEmbed().setColor('#5d83a2').setAuthor(`${message.author.tag}`, `${message.author.avatarURL()}`).setTitle('Done!').setDescription('Set the message board!');
