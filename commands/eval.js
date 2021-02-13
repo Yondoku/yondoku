95460869813fc144556ca5e1840527faeb8c1cfe
Yondoku <abcandy654@gmail.com>
2021-02-12 18:04:46 -0900

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -5,13 +5,14 @@ module.exports = {
 	args: true,
 	execute(message, args, Discord) {
 if (message.author.id !== "671264149745041408") return;
-function evaluation() {
-try {
-return eval(args.join(" "));
-} catch (err) {
-return err;
+const evaluation = () => {
+	try {
+	 return eval(args.join(" "));
+	} catch (err) {
+		return err;
+	}
 }
 const evalEmbed = new Discord.MessageEmbed().setColor('#5d83a2').setAuthor(`${message.author.tag}`, `${message.author.avatarURL()}`).setTitle("Evaluation:").setDescription(evaluation());
 message.channel.send({ embed: evalEmbed });
-			}
+},
 };
