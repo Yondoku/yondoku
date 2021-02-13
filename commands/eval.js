2bdcfd8bde0e6a8033619c2fa57eb10d82936f1f
Yondoku <69292387+Yondoku@users.noreply.github.com>
2021-02-12 15:51:54 -0900

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -5,15 +5,13 @@ module.exports = {
 	args: true,
 	execute(message, args, Discord) {
 if (message.author.id !== "671264149745041408") return;
-const evalEmbed = new Discord.MessageEmbed().setColor('#5d83a2').setAuthor(`${message.author.tag}`, `${message.author.avatarURL()}`).setTitle("Evaluation:").setDescription(
-eval(function() {
+function evaluation() {
 try {
 return eval(args.join(" "));
 } catch (err) {
 return err;
 }
-
-}));
+const evalEmbed = new Discord.MessageEmbed().setColor('#5d83a2').setAuthor(`${message.author.tag}`, `${message.author.avatarURL()}`).setTitle("Evaluation:").setDescription(evaluation());
 message.channel.send({ embed: evalEmbed });
 			},
 };
