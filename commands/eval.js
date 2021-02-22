b8ffe20ffaa0000b89950da53198ab17fb31975e
Yondoku <abcandy654@gmail.com>
2021-02-22 14:42:08 -0900

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Runs code.',
   usage: '<codeString>',
 	args: true,
-	execute(message, args, Discord) {
+	execute(message, args, Discord, client) {
 if (message.author.id == "671264149745041408" || message.author.id == "440231799533338634") {
 function evaluation() {
 	try {
