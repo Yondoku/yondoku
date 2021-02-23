e64c11ee27c3e90718dfc59613adc71404726dc6
Yondoku <abcandy654@gmail.com>
2021-02-22 17:10:49 -0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping',
 	execute(message, args, Discord, client) {
-const pingEmbed = new Discord.MessageEmbed().setColor('#5d83a2').setAuthor(`${message.author.tag}`, `${message.author.avatarURL()}`).setTitle("Pong!").setDescription(`Latency: ${Date.now() - message.createdTimestamp}ms. API Latency: ${Math.round(client.ws.ping)}ms`);
+const pingEmbed = new Discord.MessageEmbed().setColor('#5d83a2').setAuthor(`${message.author.tag}`, `${message.author.avatarURL()}`).setTitle("Pong!").setDescription(`Latency: ${Date.now() - message.createdTimestamp}ms.\nAPI Latency: ${Math.round(client.ws.ping)}ms`);
 message.channel.send({ embed: pingEmbed });
 			},
 };
